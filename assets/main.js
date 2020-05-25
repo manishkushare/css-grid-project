31bfb6abd08dafa250a96144b46ac698af7ea2f0
manish <manishkushare9@gmail.com>
2020-05-25 16:40:21 +0530

    made header transparent and changing its background after 450px

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 const formBtn = document.querySelector(".form-name");
 const btn= document.querySelector(".btn");
 const footerLogo = document.querySelector(".footer-logo");
+const header= document.querySelector(".header");
 
 function handleForm(event) {
     event.preventDefault();
@@ -15,7 +16,15 @@ function handleFooter(event) {
     event.preventDefault();
 }
 
+function handleHeader(event) {
+    if(window.scrollY > 450 ) {
+        header.style.backgroundColor= "burlywood"
+    }else {
+        header.style.backgroundColor = "transparent"
+    }
+}
+
 formBtn.addEventListener("submit",handleForm);
 btn.addEventListener("submit",handleBtn)
 footerLogo.addEventListener("click", handleFooter);
-
+window.addEventListener("scroll" ,handleHeader);
